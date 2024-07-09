833f675c035e895431fdd053040b35496e9dc1e9
FomeG <nghiavuong04@gmail.com>
2024-07-10 01:20:21 +0700

    update code

--- GUI_Quanlykhachsan/ChucNang/ADMIN/TaiChinh.Designer.cs
+++ GUI_Quanlykhachsan/ChucNang/ADMIN/TaiChinh.Designer.cs
@@ -33,7 +33,9 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // chart1
@@ -42,7 +44,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 83);
+            this.chart1.Location = new System.Drawing.Point(3, 23);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
@@ -63,18 +65,28 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "BÁO CÁO TÀI CHÍNH";
             // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.AutoScroll = true;
+            this.guna2Panel1.Controls.Add(this.chart1);
+            this.guna2Panel1.Location = new System.Drawing.Point(12, 48);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(966, 490);
+            this.guna2Panel1.TabIndex = 2;
+            // 
             // TaiChinh
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(990, 550);
+            this.Controls.Add(this.guna2Panel1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.chart1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "TaiChinh";
             this.Text = "TaiChinh";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.guna2Panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,5 +95,6 @@
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
     }
 }
