bd1b8b18dc618a06acd9e9b796a2fbe4abb970b5
FomeG <nghiavuong04@gmail.com>
2024-07-16 23:05:16 +0700

    Cập nhật lại conneection string + Namespace

--- DTO_Quanly/DB.cs
+++ DTO_Quanly/DB.cs
@@ -1,8 +1,10 @@
-﻿namespace DTO_Quanly
+﻿using DTO_Quanly.Model.DB;
+
+namespace DTO_Quanly
 {
     // Khai báo DB tĩnh 
     public static class DTODB
     {
-        public static DuAn1_QLKS db = new DuAn1_QLKS();
+        public static DuAnQLKS db = new DuAnQLKS();
     }
 }
